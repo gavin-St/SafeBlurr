edc2d39bd40f7c0cd76cf7555ac4e5137e8548a6
Justin Lo <justin.tzehing.lo@gmail.com>
2023-11-18 16:10:36 -0500

    audio mixer

--- pitch-change/audio-mixer-test.py
+++ pitch-change/audio-mixer-test.py
@@ -1,23 +1,13 @@
 from pydub import AudioSegment
-from pydub.playback import play
-import os
+from pydub.effects import pitch_shift
 
-# pip install pydub
+audio = AudioSegment.from_file("video_sound.mp3", format="mp3")
 
-file_path = "data/test-video.mp4"
-if not os.path.exists(file_path):
-    print(f"File not found: {file_path}")
-else:
-    print("Yes")
-    audio = AudioSegment.from_file(file_path, format="mp4")
+# Define the pitch shift factor (positive or negative)
+pitch_factor = -3  # Adjust this value according to your preference
 
-    # Set the desired pitch shift factor (1.0 means no change)
-    pitch_factor = 1.5  # Adjust as needed
+# Apply pitch shift
+shifted_audio = pitch_shift(audio, pitch_factor)
 
-    # Apply the pitch shift
-    audio_pitch_shifted = audio._spawn(audio.raw_data, overrides={
-        "frame_rate": int(audio.frame_rate * pitch_factor)
-    })
-
-    # Export the modified audio
-    audio_pitch_shifted.export("output_pitch_shifted.mp3", format="mp3")
+# Save the modified audio
+shifted_audio.export("output_file.mp3", format="mp3")
